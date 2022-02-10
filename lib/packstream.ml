79f45200b07529cf10c3a604a1ac910af43894a5
Tomasz Barański <tomasz.baranski@posteo.net>
2022-02-10 22:49:14 +0100

    Parse one-byte ints

--- lib/packstream.ml
+++ lib/packstream.ml
@@ -2,10 +2,12 @@ type message =
   | Null
   | True
   | False
+  | Int of int
 
 let parse bitstring =
   match%bitstring bitstring with
   | {| 0xC0 : 8 |} -> Ok Null
   | {| 0xC2 : 8 |} -> Ok False
   | {| 0xC3 : 8 |} -> Ok True
+  | {| i : 8 : int,signed |} -> Ok (Int i)
   | {| _ |} -> Error "Invalid message"
--- test/packstream.ml
+++ test/packstream.ml
@@ -13,11 +13,42 @@ let test_true _ctx =
   let bs = Bitstring.bitstring_of_string "\xc3" in
   assert_equal (Ok True) (parse bs)
 
+let test_tiny_int_negative (str, v) =
+  let bs = Bitstring.bitstring_of_string str in
+  assert_equal (Ok (Int v)) (parse bs)
+
+let test_tiny_int_positive i =
+  let%bitstring bs = {| i : 8 |} in
+  assert_equal (Ok (Int i)) (parse bs)
+
+let test_tiny_int _ctx =
+  let negative = [
+    ("\xf0", -16);
+    ("\xf1", -15);
+    ("\xf2", -14);
+    ("\xf3", -13);
+    ("\xf4", -12);
+    ("\xf5", -11);
+    ("\xf6", -10);
+    ("\xf7", -9);
+    ("\xf8", -8);
+    ("\xf9", -7);
+    ("\xfa", -6);
+    ("\xfb", -5);
+    ("\xfc", -4);
+    ("\xfd", -3);
+    ("\xfe", -2);
+    ("\xff", -1);
+  ] in
+  List.iter test_tiny_int_negative negative;
+  List.iter test_tiny_int_positive (List.init 128 Fun.id)
+
 let all_tests =
   "all_tests" >:::
   ["test_null" >:: test_null;
    "test_false" >:: test_false;
-   "test_true" >:: test_true]
+   "test_true" >:: test_true;
+   "test_tiny_int" >:: test_tiny_int]
 
 
 let () =
